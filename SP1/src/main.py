cc4b4dd7ad939497950d8a51333e92c7f5b3b248
SpeekeR <DominikZappe1999@seznam.cz>
2024-03-06 20:17:39 +0100

    Basic rules are working, assimilation is going to be pain now

--- SP1/src/main.py
+++ SP1/src/main.py
@@ -1,3 +1,10 @@
+import json
+
+with open("../data/epa.json", "r", encoding="utf-8") as file:
+    rules = json.load(file)
+
+print(list(rules.keys()))
+
 data = []
 with open("../data/kniha.ortho.txt", "r", encoding="utf-8") as file:
     for line in file:
@@ -16,3 +23,24 @@ result += "$|"
 
 print(sentence)
 print(result)
+
+for rule in rules["zakladni_pravidla"]:
+    if rule == "y" or rule == "ý":
+        continue
+    result = result.replace(rule, rules["zakladni_pravidla"][rule])
+
+for rule in rules["spojeni_samohlasek"]:
+    result = result.replace(rule, rules["spojeni_samohlasek"][rule])
+
+for rule in rules["spojeni_souhlasky_a_samohlasky"]:
+    if rule == "symbolic_rules":
+        continue
+    result = result.replace(rule, rules["spojeni_souhlasky_a_samohlasky"][rule])
+
+for rule in rules["zakladni_pravidla"]:
+    if rule == "y" or rule == "ý":
+        result = result.replace(rule, rules["zakladni_pravidla"][rule])
+
+print(result)
+
+print("|$|vZdicki|si|pQAl|mId|hospodu|#|!ale|nemJel|rAt|liDi|$|")
