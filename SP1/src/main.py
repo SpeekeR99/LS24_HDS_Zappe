8c45bb112d4825ff60f8f05a43d814a17b9b9007
SpeekeR <DominikZappe1999@seznam.cz>
2024-03-11 19:54:51 +0100

    Finalized the code, documentation and comments added

--- SP1/src/main.py
+++ SP1/src/main.py
@@ -1,14 +1,31 @@
+import sys
 import json
 from phntrn import apply_rule
 
 
-def load_files(epa_path="../data/epa.json", rules_path="../data/rules.json", data_path="../data/vety_HDS.ortho.txt"):
+EPA_FP = "../data/epa.json"
+RULES_FP = "../data/rules.json"
+INPUT_FP = "../data/vety_HDS.ortho.txt"
+OUTPUT_FP = "../data/vety_HDS.phntrn.txt"
+
+
+def load_files(epa_path=EPA_FP, rules_path=RULES_FP, data_path=INPUT_FP):
+    """
+    Load all the necessary files (epa.json, rules.json and a file with orthographic sentences to transform)
+    :param epa_path: File path to epa.json
+    :param rules_path: File path to rules.json
+    :param data_path: File path to orthographic sentences (to be transformed)
+    :return: EPA dictionary, Rules dictionary, List of orthographic sentences
+    """
+    # Load epa.json
     with open(epa_path, "r", encoding="utf-8") as fp:
         epa = json.load(fp)
 
+    # Load rules.json
     with open(rules_path, "r", encoding="utf-8") as fp:
         rules = json.load(fp)
 
+    # Load orthographic sentences
     data = []
     with open(data_path, "r", encoding="utf-8") as file:
         for line in file:
@@ -17,31 +34,65 @@ def load_files(epa_path="../data/epa.json", rules_path="../data/rules.json", dat
     return epa, rules, data
 
 
-def main(output_path="../data/vety_HDS.phntrn.txt"):
-    epa, rules, data = load_files()
+def main(input_path=INPUT_FP, output_path=OUTPUT_FP):
+    """
+    Main function to do all the transformation and hard work
+    :param input_path: Input file path for the orthographic sentences
+    :param output_path: Output file path for the phonetically transcribed sentences
+    """
+    # Load all the necessary files
+    epa, rules, data = load_files(data_path=input_path)
+    print("Phonetic transcription in progress...")
 
-
+    # Apply all the rules to the orthographic sentences
     phntrn = []
-    for sentence in data:
+    for sentence in data:  # For each orthographic sentence
+        # Add start and end symbols and make sure the sentence is lower case
         sentence = "|$|" + sentence.lower() + "|$|"
 
-        sentence = apply_rule(sentence, epa, rules, "basic")
-        sentence = apply_rule(sentence, epa, rules, "2.8.3")
-        sentence = apply_rule(sentence, epa, rules, "2.8.5")
-        sentence = apply_rule(sentence, epa, rules, "2.8.6")
-        sentence = apply_rule(sentence, epa, rules, "2.8.7.1", repeat_rule=5)
-        sentence = apply_rule(sentence, epa, rules, "2.8.7.2")
-        sentence = apply_rule(sentence, epa, rules, "2.8.7.3")
-        sentence = apply_rule(sentence, epa, rules, "2.8.7.4")
-        sentence = apply_rule(sentence, epa, rules, "2.8.3", ignore_y_rule=False)
-        sentence = apply_rule(sentence, epa, rules, "2.8.4")
+        # Apply all the rules
+        sentence, _ = apply_rule(sentence, epa, rules, "basic")
+        sentence, _ = apply_rule(sentence, epa, rules, "2.8.3")
+        sentence, _ = apply_rule(sentence, epa, rules, "2.8.5")
+        sentence, _ = apply_rule(sentence, epa, rules, "2.8.6")
+        sentence, changed = apply_rule(sentence, epa, rules, "2.8.7.1")
+        while changed:  # Keep applying the assimilation rule until it does not change the sentence anymore
+            sentence, changed = apply_rule(sentence, epa, rules, "2.8.7.1")
+        sentence, _ = apply_rule(sentence, epa, rules, "2.8.7.2")
+        sentence, _ = apply_rule(sentence, epa, rules, "2.8.7.3")
+        sentence, _ = apply_rule(sentence, epa, rules, "2.8.7.4")
+        sentence, _ = apply_rule(sentence, epa, rules, "2.8.3", ignore_y_rule=False, ignore_x_rule=True)
+        sentence, _ = apply_rule(sentence, epa, rules, "2.8.4")
 
         phntrn.append(sentence)
 
+    # Save the phonetically transcribed sentences into an output file
     with open(output_path, "w", encoding="utf-8") as fp:
         for sentence in phntrn:
             fp.write(sentence + "\n")
 
+    print("Phonetically transcribed sentences saved to " + output_path)
+
 
 if __name__ == "__main__":
-    main()
+    if len(sys.argv) == 1:  # No arguments
+        print("Using default file paths...")
+        print("\t - EPA: " + EPA_FP)
+        print("\t - Rules: " + RULES_FP)
+        print("\t - Input: " + INPUT_FP)
+        print("\t - Output: " + OUTPUT_FP)
+        main()
+    elif len(sys.argv) == 3:  # Two arguments
+        print("Using specified file paths...")
+        print("\t - EPA: " + EPA_FP)
+        print("\t - Rules: " + RULES_FP)
+        print("\t - Input: " + sys.argv[1])
+        print("\t - Output: " + sys.argv[2])
+        main(input_path=sys.argv[1], output_path=sys.argv[2])
+    else:  # Anything else
+        print("Invalid number of arguments!")
+        print("Usage: python " + sys.argv[0])
+        print("Usage: python " + sys.argv[0] + " <input_file> <output_file>")
+        print("\t - <input_file>: File path to orthographic sentences")
+        print("\t - <output_file>: File path to save phonetically transcribed sentences to")
+        sys.exit(1)
--- SP1/src/phntrn.py
+++ SP1/src/phntrn.py
@@ -1,27 +1,51 @@
 import itertools
 
 
-def apply_rule(sentence, epa, rules, rule_name, repeat_rule=1, ignore_y_rule=True):
-    for rule in rules[rule_name]:
-        for _ in range(repeat_rule):
-            if (rule == "y" or rule == "ý") and ignore_y_rule:
-                continue
+def apply_rule(sentence, epa, rules, rule_name, ignore_y_rule=True, ignore_x_rule=False):
+    """
+    Applies a rule to a sentence
+    :param sentence: Sentence to apply the rule to
+    :param epa: EPA dictionary from epa.json
+    :param rules: Rules dictionary from rules.json
+    :param rule_name: Rule to apply
+    :param ignore_y_rule: Special case for rule 2.8.3
+    :param ignore_x_rule: Special case for rule 2.8.3
+    :return: Changed sentence and whether the sentence was changed by the rule or not
+    """
+    original = sentence
 
-            if rule == "symbolic":
-                for sym_rule in rules[rule_name][rule]:
-                    sym_value = rules[rule_name][rule][sym_rule]
-                    sentence = _apply_symbolic_rule(sentence, sym_rule, sym_value, epa)
+    for rule in rules[rule_name]:  # For each rule in the set of rules
+        if (rule == "y" or rule == "ý") and ignore_y_rule:  # Special case for rule 2.8.3
+            continue
+        if rule == "x" and ignore_x_rule:  # Special case for rule 2.8.3
+            continue
 
-            else:
-                sentence = sentence.replace(rule, rules[rule_name][rule])
+        if rule == "symbolic":  # Special case of symbolic rules
+            for sym_rule in rules[rule_name][rule]:  # For each symbolic rule in the set of symbolic rules
+                sym_value = rules[rule_name][rule][sym_rule]
+                sentence = _apply_symbolic_rule(sentence, sym_rule, sym_value, epa)
 
-    return sentence
+        else:  # Normal rules
+            sentence = sentence.replace(rule, rules[rule_name][rule])
+
+    # Check if the sentence was changed and return the changed sentence with a boolean
+    changed = sentence != original
+    return sentence, changed
 
 
 def _apply_symbolic_rule(sentence, lvalue, rvalue, epa):
+    """
+    Applies a symbolic rule to a sentence
+    :param sentence: Sentence to apply the rule to
+    :param lvalue: Left side of the rule
+    :param rvalue: Right side of the rule
+    :param epa: EPA dictionary from epa.json
+    :return: Changed sentence
+    """
     l_symbols = []
     r_symbols = []
-    # symbols are between "<" and ">"
+
+    # Symbols are between "<" and ">", parse them
     for i in range(len(lvalue)):
         if lvalue[i] == "<":
             start = i
@@ -35,10 +59,11 @@ def _apply_symbolic_rule(sentence, lvalue, rvalue, epa):
             end = i
             r_symbols.append(rvalue[start + 1:end])
 
+    # If there are no symbols or the first symbol is "COM" (for comments), do nothing
     if len(l_symbols) == 0 or l_symbols[0] == "COM":
         return sentence
 
-    # substitute symbols with possible values
+    # Substitute symbols with possible values (from EPA)
     l_subs = []
     r_subs = []
     for symbol in l_symbols:
@@ -46,16 +71,18 @@ def _apply_symbolic_rule(sentence, lvalue, rvalue, epa):
     for symbol in r_symbols:
         r_subs.append(epa[symbol])
 
-    # generate all possible combinations of symbols
+    # Generate all possible combinations of symbols
     l_comb = list(itertools.product(*l_subs))
     r_comb = list(itertools.product(*r_subs))
 
+    # Replace symbols with values from the combinations
+    # This works because the symbols in the left and right side are in the same order
+    # eg. "b" has the same index as "p", thus making a pair
     l_subs = []
     r_subs = []
     for combination in l_comb:
         temp = lvalue
         for i in range(len(combination)):
-            # replace first occurrence of symbol with value
             temp = temp.replace("<" + l_symbols[i] + ">", combination[i], 1)
         l_subs.append(temp)
     for combination in r_comb:
@@ -64,6 +91,7 @@ def _apply_symbolic_rule(sentence, lvalue, rvalue, epa):
             temp = temp.replace("<" + r_symbols[i] + ">", combination[i], 1)
         r_subs.append(temp)
 
+    # Replace the left side with the right side in the sentence
     for i in range(len(l_subs)):
         sentence = sentence.replace(l_subs[i], r_subs[i])
 
